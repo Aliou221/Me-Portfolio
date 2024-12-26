d291a84e1b60e54bde297ffc9a790afee79316a6
Papiscocisse <143758558+Aliou221@users.noreply.github.com>
2024-12-26 02:18:27 +0000

    Update scroll.js

--- JS/scroll.js
+++ JS/scroll.js
@@ -1,23 +1,23 @@
-let section = document.querySelectorAll("section"); //Recuperation de tout mes sections
-let navlinks = document.querySelectorAll("nav ul li a")//Recuperation de tous mes liens de navigation
+let section = document.querySelectorAll("section"); 
+let navlinks = document.querySelectorAll("nav ul li a")
 
-window.addEventListener("scroll", ()=>{ //evenment scroll
+window.addEventListener("scroll", ()=>{ 
     
-    section.forEach(sec =>{ //Parcourir tous mes section
+    section.forEach(sec =>{ 
 
-        let tete = window.scrollY; //Position actuel de mon fenetre par rapport en haut de page 
-        let hauteur = sec.offsetHeight; //La hauteur de la section qui est en cours 
-        let pos_section = sec.offsetTop - 150; //Position de mon section par rapport au haut de page
-        let id = sec.getAttribute("id");  //id de la section utiliser pour voir le lien correspondant
+        let tete = window.scrollY; 
+        let hauteur = sec.offsetHeight; 
+        let pos_section = sec.offsetTop - 150; 
+        let id = sec.getAttribute("id");  
 
 
-        if(tete >= pos_section && tete < pos_section + hauteur){ //Verification si la section est visible ou pas
+        if(tete >= pos_section && tete < pos_section + hauteur){ 
             
-            navlinks.forEach(link =>{ //parcurir les liens 
+            navlinks.forEach(link =>{
 
-                link.classList.remove("active")//suprime la class 'active' de tous les liens 
+                link.classList.remove("active")
 
-                document.querySelector('nav ul li a[href*='+id+']').classList.add("active") //Voir le lien qui a pour href l'id du section et on ajoute la class "active" sur cette lien
+                document.querySelector('nav ul li a[href*='+id+']').classList.add("active")
             
             })
         }
