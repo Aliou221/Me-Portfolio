35c179d55ecc249b3668909a85a399b7768f4951
Papiscocisse <143758558+Aliou221@users.noreply.github.com>
2024-12-27 19:54:43 +0000

    Update service.js

--- JS/service.js
+++ JS/service.js
@@ -7,3 +7,65 @@ document.addEventListener("scroll",()=>{
         header.style.backgroundColor = "transparent"
     }
 })
+
+let imgNones = document.querySelectorAll(".img-none")
+let btnImg = document.querySelector(".btn-img")
+
+let cont = 0;
+
+
+btnImg.onclick = function(){
+
+
+    if(cont <= 3){
+
+        imgNones.forEach((img,i) =>{
+
+            if(cont === 0){
+                if(i>=0 && i < 5){
+                    img.classList.add("img-visible")
+                }
+            }
+
+            if(cont === 1){
+                if(i>=5 && i < 10){
+                    img.classList.add("img-visible")
+                }
+            }
+
+            if(cont === 2){
+                if(i>=10 && i < 15){
+                    img.classList.add("img-visible")
+                }
+            }
+
+            if(cont === 3){
+                if(i>=15 && i < 17){
+                    btnImg.innerText = "Voir moins"
+                    img.classList.add("img-visible")
+                }
+            }
+
+        })
+
+        cont++;
+
+    }else{
+
+        cont = -1
+
+        if(cont === -1){
+
+            imgNones.forEach((img,i) =>{
+    
+                if(i>=0 && i < 19){
+                    
+                    btnImg.innerText = "Voir plus"
+                    img.classList.remove("img-visible")
+                }
+            })
+        }
+
+    }
+
+}
